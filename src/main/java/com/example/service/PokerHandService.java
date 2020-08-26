36ffd2790fb88d67cf99a1d69376f0761e6afa88
y24er <1028704486@qq.com>
2020-08-26 21:21:09 +0800

    [poker-hands][Bruno/Jessie] refactor: refactor pokerHand method

--- src/main/java/com/example/service/PokerHandService.java
+++ src/main/java/com/example/service/PokerHandService.java
@@ -3,20 +3,21 @@ package com.example.service;
 import com.example.domain.Poker;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PokerHandService {
-
+    Comparator<String> comparator =new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+            return Integer.valueOf(o1).compareTo(Integer.valueOf(o2));
+        }
+    };
     public String pokerHand(List<Poker> playerOnePokers, List<Poker> playerTwoPokers) {
-
-        List<String> idListOne = playerOnePokers.stream().map(Poker::getId).collect(Collectors.toList());
-        List<Integer> idNewListOne = idListOne.stream().map(Integer::valueOf).collect(Collectors.toList());
-
-        List<String> idListTwo = playerTwoPokers.stream().map(Poker::getId).collect(Collectors.toList());
-        List<Integer> idNewListTwo = idListTwo.stream().map(Integer::valueOf).collect(Collectors.toList());
-
-        return Collections.max(idNewListOne) > Collections.max(idNewListTwo) ? "player1" : "player2";
+        String idListOne = playerOnePokers.stream().map(Poker::getId).max(comparator).get();
+        String idListTwo = playerTwoPokers.stream().map(Poker::getId).max(comparator).get();
+        return comparator.compare(idListOne,idListTwo)>0 ? "player1" : "player2";
     }
 
 
