0ed9a5b602551e23bd27b0be4162f323c712b063
y24er <1028704486@qq.com>
2020-08-26 21:46:41 +0800

    [poker-hands][Bruno/Jessie] feature:implement pokerHandPair method

--- src/main/java/com/example/service/PokerHandService.java
+++ src/main/java/com/example/service/PokerHandService.java
@@ -2,29 +2,29 @@ package com.example.service;
 
 import com.example.domain.Poker;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PokerHandService {
-    Comparator<String> comparator =new Comparator<String>() {
+    Comparator<String> comparator = new Comparator<String>() {
         @Override
         public int compare(String o1, String o2) {
             return Integer.valueOf(o1).compareTo(Integer.valueOf(o2));
         }
     };
+
     public String pokerHand(List<Poker> playerOnePokers, List<Poker> playerTwoPokers) {
         String idListOne = playerOnePokers.stream().map(Poker::getId).max(comparator).get();
         String idListTwo = playerTwoPokers.stream().map(Poker::getId).max(comparator).get();
-        return comparator.compare(idListOne,idListTwo)>0 ? "player1" : "player2";
+        return comparator.compare(idListOne, idListTwo) > 0 ? "player1" : "player2";
     }
 
 
     public String pokerHandPair(List<Poker> playerOnePokers, List<Poker> playerTwoPokers) {
-
-        playerOnePokers.stream().map(Poker::getId);
-
-        return "";
+        Map<String, Long> counterOne = playerOnePokers.stream().map(Poker::getId).collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+        Map<String, Long> counterSecond = playerTwoPokers.stream().map(Poker::getId).collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+        return counterOne.size()>counterSecond.size()?"player2":"player1";
     }
 }
